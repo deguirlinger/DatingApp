a7862cec6367157cdc4f6f0f9f979593b8163ed2
Doug <deguirlinger@gmail.com>
2019-09-26 22:19:19 -0700

    updated api for edit profile

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,10 +1,9 @@
 using System.Linq;
 using AutoMapper;
 using DatingApp.API.Dtos;
-using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 
-namespace DatingApp.API.Controllers
+namespace DatingApp.API.Helpers
 {
     public class AutoMapperProfiles : Profile
     {
@@ -37,6 +36,7 @@ namespace DatingApp.API.Controllers
                     )
                 );
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
