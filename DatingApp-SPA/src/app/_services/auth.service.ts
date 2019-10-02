52d6e25fa6f216765dc4844f3c6d03839dee9892
Doug <deguirlinger@gmail.com>
2019-10-02 13:53:12 -0700

    added main photo to navbar

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -19,6 +21,8 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          localStorage.setItem('user', JSON.stringify(user.user));
+          this.currentUser = user.user;
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
           console.log(this.decodedToken);
         }
